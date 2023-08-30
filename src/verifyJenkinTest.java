600122dde45cab4ea8f91fe7aa5d956c2020a8c1
RashmiBhat <rbhat@tekion.com>
2023-08-30 20:01:44 +0530

    added 10tc

---
+++ src/verifyJenkinTest.java
@@ -0,0 +1,71 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class verifyJenkinTest {
+
+    @Test
+    public void test1()
+    {
+        System.out.println("test1");
+        Assert.assertTrue(false);
+    }
+    @Test
+    public void test2()
+    {
+        System.out.println("test2");
+        Assert.assertTrue(false);
+    }
+    @Test
+    public void test3()
+    {
+        System.out.println("test3");
+        Assert.assertTrue(false);
+    }
+    @Test
+    public void test4()
+    {
+        System.out.println("test4");
+        Assert.assertTrue(false);
+    }
+    @Test
+    public void test5()
+    {
+        System.out.println("test5");
+        Assert.assertTrue(false);
+    }
+    @Test
+    public void test6()
+    {
+        System.out.println("test6");
+        Assert.assertTrue(true);
+    }
+    @Test
+    public void test7()
+    {
+        System.out.println("test7");
+        Assert.assertTrue(true);
+    }
+    @Test
+    public void test8()
+    {
+        System.out.println("test8");
+        Assert.assertTrue(true);
+    }
+    @Test
+    public void test9()
+    {
+        System.out.println("test9");
+        Assert.assertTrue(true);
+    }
+    @Test
+    public void test10()
+    {
+        System.out.println("test10");
+        Assert.assertTrue(true);
+    }
+
+
+}
