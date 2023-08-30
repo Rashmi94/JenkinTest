61a95b1fc940df839045385517c9cb519377d5f9
RashmiBhat <rbhat@tekion.com>
2023-08-30 21:58:24 +0530

    removed unused packages

--- src/verifyJenkinTest.java
+++ src/verifyJenkinTest.java
@@ -1,7 +1,4 @@
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class verifyJenkinTest {
