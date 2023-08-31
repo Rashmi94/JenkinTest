4a50426defa18245d7b76573163a02c4611d120c
RashmiBhat <rbhat@tekion.com>
2023-08-31 14:35:28 +0530

    updated 2

--- src/verifyJenkinTest.java
+++ src/verifyJenkinTest.java
@@ -8,31 +8,31 @@ public class verifyJenkinTest {
     public void test1()
     {
         System.out.println("test1");
-        Assert.assertTrue(false);
+        Assert.assertTrue(true);
     }
     @Test
     public void test2()
     {
         System.out.println("test2");
-        Assert.assertTrue(false);
+        Assert.assertTrue(true);
     }
     @Test
     public void test3()
     {
         System.out.println("test3");
-        Assert.assertTrue(false);
+        Assert.assertTrue(true);
     }
     @Test
     public void test4()
     {
         System.out.println("test4");
-        Assert.assertTrue(false);
+        Assert.assertTrue(true);
     }
     @Test
     public void test5()
     {
         System.out.println("test5");
-        Assert.assertTrue(false);
+        Assert.assertTrue(true);
     }
     @Test
     public void test6()
